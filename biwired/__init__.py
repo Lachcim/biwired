765cba85938ee38a2a7f79d9b0d440aa0c82a124
Michał Szopiński <jajnick@hotmail.com>
2020-04-02 22:27:27 +0200

    Add driver initialization

---
+++ biwired/__init__.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+
+class Biwired:  
+    def __init__(self):
+        self.driver = None
+        
+    def start_driver(self, driver=None):
+        # provide default driver
+        if not driver:
+            options = webdriver.ChromeOptions()
+            options.add_argument("headless")
+            options.add_argument("log-level=3")
+            options.add_argument("user-agent=Mozilla/5.0 Chrome/80.0 Biwired/0.1")
+            options.add_experimental_option("excludeSwitches", ["enable-logging"])
+            
+            driver = webdriver.Chrome(chrome_options=options)
+        
+        # start: request login screen
+        self.driver = driver
+        self.driver.get("https://app.wire.com/auth/#login")
