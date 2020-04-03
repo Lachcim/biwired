a574e52b68e8803280b906bb6f726b8a8b3cf80b
Michał Szopiński <jajnick@hotmail.com>
2020-04-03 16:00:35 +0200

    Implement asset repository

--- biwired/events.py
+++ biwired/events.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from biwired.assets import Asset
 
 class BiwiredEvent: 
     def __init__(self, parent=None, raw_event=None):
@@ -16,22 +17,72 @@ class BiwiredEvent:
             else:
                 setattr(self, key, value)
                 
+        # handle assets
+        if self.type == "new_asset":
+            # create new asset
+            asset = Asset(self.parent, self.id)
+            asset.name = self.file_name
+            asset.size = self.file_size
+            asset.mime_type = self.file_mime_type
+            
+            # add new asset to repository
+            self.parent.assets[self.id] = asset
+            
+            # recreate attributes
+            del self.file_name
+            del self.file_size
+            del self.file_mime_type
+            self.asset = self.id
+        elif self.type == "asset_finished":
+            # update entry in repository
+            self.parent.assets[self.asset].success = self.success
+            self.parent.assets[self.asset].key = self.key
+            self.parent.assets[self.asset].token = self.token
+                
     def __str__(self):
         # format time, question mark for unknown times
         time = self.time.strftime("%Y-%m-%d %H:%M:%S") if self.time else "?"
         header = "{0} [#{1}]".format(time, self.id[:5])
     
         if self.type == "new_message":
-            return "{0} #{1} sent message to #{2}: {3}".format(header, self.author[:5], self.conversation[:5], self.content)
+            return "{0} #{1} sent message to #{2}: {3}".format(header,
+                                                               self.author[:5],
+                                                               self.conversation[:5],
+                                                               self.content)
+        elif self.type == "new_asset":
+            return "{0} #{1} started uploading asset to #{2}".format(header,
+                                                               self.author[:5],
+                                                               self.conversation[:5])
+        elif self.type == "asset_finished":
+            if self.success:
+                return "{0} #{1} finished uploading asset #{2}".format(header,
+                                                       self.author[:5],
+                                                       self.asset[:5])
+            else:
+                return "{0} #{1} failed to upload asset #{2}".format(header,
+                                                       self.author[:5],
+                                                       self.asset[:5])
+                                                               
         elif self.type == "message_edited":
-            return "{0} #{1} edited message #{2}: {3}".format(header, self.author[:5], self.message[:5], self.content)
+            return "{0} #{1} edited message #{2}: {3}".format(header,
+                                                              self.author[:5],
+                                                              self.message[:5],
+                                                              self.content)
         elif self.type == "message_delivered":
-            return "{0} #{1} received message #{2}".format(header, self.reader[:5], self.message[:5])
+            return "{0} #{1} received message #{2}".format(header,
+                                                           self.reader[:5],
+                                                           self.message[:5])
         elif self.type == "reaction_added":
-            return "{0} #{1} reacted to message #{2}".format(header, self.reactor[:5], self.message[:5])
+            return "{0} #{1} reacted to message #{2}".format(header,
+                                                             self.reactor[:5],
+                                                             self.message[:5])
         elif self.type == "reaction_removed":
-            return "{0} #{1} unreacted to message #{2}".format(header, self.reactor[:5], self.message[:5])
+            return "{0} #{1} unreacted to message #{2}".format(header,
+                                                               self.reactor[:5],
+                                                               self.message[:5])
         elif self.type == "unknown": 
-            return "{0} unknown event: {1} {2}".format(header, self.raw_type, self.raw_data)
+            return "{0} unknown event: {1} {2}".format(header,
+                                                       self.raw_type,
+                                                       self.raw_data)
         else:
             return "invalid event"
--- biwired/js/subscribe.js
+++ biwired/js/subscribe.js
@@ -19,6 +19,38 @@ amplify.subscribe("wire.webapp.conversation.event_from_backend", function(rawEve
 			event.message = rawEvent.data.replacing_message_id;
 		}
 	}
+	else if (rawEvent.type == "conversation.asset-add") {
+		event.type = "new_asset"
+		event.author = rawEvent.from;
+		event.conversation = rawEvent.conversation;
+		
+		//asset upload start vs finish
+		if (!window.biwired_takenIds.has("asset" + event.id)) {
+			event.file_size = rawEvent.data.content_length;
+			event.file_mime_type = rawEvent.data.content_length;
+			event.file_name = rawEvent.data.info.name;
+			
+			window.biwired_takenIds.add("asset" + event.id)
+		}
+		else {
+			event.type = "asset_finished";
+			event.asset = event.id;
+			
+			//mock new id
+			var charCode = event.id.charCodeAt(0) + 1;
+			if (charCode == 58 || charCode == 123) charCode = 48;
+			event.id = String.fromCharCode(charCode) + event.id.substr(1);
+			event.id[8] = "X"; //mark counterfeit id
+			
+			//suppress irrelevant states
+			if (rawEvent.data.status != "uploaded" && rawEvent.data.status != "upload-failed")
+				return;
+			
+			event.success = rawEvent.data.status == "uploaded";
+			event.key = rawEvent.data.key || null;
+			event.token = rawEvent.data.token || null;
+		}
+	}
 	else if (rawEvent.type == "conversation.confirmation") {
 		event.type = "message_delivered";
 		event.reader = rawEvent.from;
