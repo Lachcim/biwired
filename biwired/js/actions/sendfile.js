a3822cac73139f579a867af6e682df0c6696d650
Michał Szopiński <jajnick@hotmail.com>
2020-04-07 16:16:06 +0200

    Improve file uploading

--- biwired/js/actions/sendfile.js
+++ biwired/js/actions/sendfile.js
@@ -15,20 +15,48 @@ if (!conversation) {
 	return;
 }
 
+if (!repo.biwired_injected) {
+	//create copies of original methods
+	repo.send_asset_metadata_old = repo.send_asset_metadata;
+	repo._on_asset_upload_complete_old = repo._on_asset_upload_complete;
+	
+	//wire doesn't create a confirmation event by default
+	//create proxy method to fabricate event
+	repo._on_asset_upload_complete = function(convo, rawEvent) {
+		var event = {};
+		event.type = "new_asset";
+		event.time = new Date(rawEvent.time).getTime() / 1000;
+		event.id = rawEvent.id;
+		event.author = rawEvent.from;
+		event.conversation = rawEvent.conversation;
+		event.status = "uploaded";
+		event.key = rawEvent.data.key;
+		event.token = rawEvent.data.token;
+		
+		window.biwired_events.push(event);
+		
+		return repo._on_asset_upload_complete_old(convo, rawEvent);
+	}
+	
+	repo.biwired_injected = true;
+}
+
 //create proxy method to obtain message id
-var uploadEvent;
-var sam = repo.send_asset_metadata;
 repo.send_asset_metadata = function() {
 	var proxyArgs = arguments;
 	
 	return new Promise(function(resolve) {
-		sam.apply(repo, proxyArgs).then(function(event) {
+		repo.send_asset_metadata_old(...proxyArgs).then(function(event) {
 			//return message id to Python, continue uploading
 			callback(event.id);
-			uploadEvent = event;
 			resolve(event);
+		},
+		function(resolve) {
+			//on error
+			callback(null);
+			resolve()
 		});
 	});
 }
 
-repo.upload_file(conversation, window.biwired_fileInput.files[0], arguments[1])
+repo.upload_file(conversation, window.biwired_fileInput.files[0], arguments[1]);
